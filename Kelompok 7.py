0564fbb831c938a36e66a9e1d91fb27f67e5f7f0
RANINAYLATSANIANAJWA <najwassofwa@gmail.com>
2024-05-20 20:40:29 +0700

    Add files via upload

---
+++ Kelompok 7.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.write("Website ini dirancang untuk mempermudah tenaga medis untuk memprediksi penyakit diabetes pada pasien")
+st.title("Website ini dirancang oleh")
+st.write("1. Fitri Syahrani Prasetyaningrum (2360129)")
+st.write("2. Muhammad Ariel. Z (2360181)")
+st.write("3. Najwa Nubzatussofwa(2360201)")
+st.write("4. Nayla Khairunnisa (2360208)")
+st.write("5. Tsania Zahra (2360280)")
+st.image("WhatsApp.jpg")
